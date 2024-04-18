6eb8779bde230d11c992387d0039bd4bead9d240
AnkitBali <18bcs015@smvdu.ac.in>
2024-04-18 18:48:40 +0530

    chapter 16 done

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,9 @@
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 export default function Page() {
-    return <p>Customers Page</p>;
+  return <CustomersTable customers={[]} />;
 }
